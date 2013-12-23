2fd0ebd5f20dcdb5c397fc726a918057a0149603
nextime <franco@unixmedia.it>
2013-12-23 01:19:35 +0100

    Permit webapp add to homescreen for android/chrome >= M31

--- Web/htdocs/gui/parts/head.php
+++ Web/htdocs/gui/parts/head.php
@@ -2,7 +2,9 @@
 <title>Domotika GUI - <?=$tr->Get($GUISECTION)?></title>  
    <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="mobile-web-app-capable" content="yes">
     <? /*
+    <link rel="shortcut icon" sizes="196x196" href="/icon.png">
     <!-- Bootstrap -->
     <link href="/resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
     <link href="/resources/glyphicons/css/bootstrap-glyphicons.css" rel="stylesheet" media="screen">
