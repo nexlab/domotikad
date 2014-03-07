7d03dffb1d3cc38c4923ecdd183581b2fd9e5872
nextime <franco@unixmedia.it>
2014-03-07 12:11:55 +0100

    Added text2speechfile.sh script

---
+++ scripts/text2speechfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#cd `dirname $0`/../domotika/clouds/google
+
+if [[ $# < "3" ]] ; then
+   echo "Usage:   $0 <destination file> <language> \'<text>\'"
+   echo ""
+   echo "Example: $0 foo.wav it prova di trasmissione"
+   echo ""
+   exit 0
+fi
+
+
+
+python `dirname $0`/../domotika/clouds/google/tts.py $1 $2 "${3}"
